11700eb8680ed21e23fdd5bab7097a799b0b28a5
Daniel Miller <danielmiller.nl@gmail.com>
2015-02-13 21:48:40 -0330

    enemies drawn as svg elements

--- watchout.js
+++ watchout.js
@@ -1,2 +1,16 @@
 // start slingin' some d3 here.
+var gameBoard = d3.select(".container").append("svg").attr("width",800).attr("height",400);
 
+var enemyData = [];
+for (var i = 0; i < 20; i++) {
+  enemyData[i] = 0;
+}
+
+var enemies = gameBoard.selectAll(".enemies")
+                  .data(enemyData)
+                  .enter()
+                .append("circle")
+                  .attr("cx", function(d) {return 800 * Math.random();})
+                  .attr("cy", function(d) {return 400 * Math.random();})
+                  .attr("r",20)
+                  .attr("fill","blue");
